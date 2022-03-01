06e8c87033044ecbc908b13960cd29be65edb8a9
Michael Legleux <legleux@users.noreply.github.com>
2022-03-01 12:03:21 -0800

    Quiet postgres (#107)
    * set cxx_std_20 only for libclio
    
    * Be explicit with requiring ExternalProject
    
    * Specify Ubuntu 20.04's cmake version exactly
    
    * quiet postgres install step

--- CMake/settings.cmake
+++ CMake/settings.cmake
@@ -1,5 +1,1 @@
-set(CMAKE_CXX_EXTENSIONS OFF)
-set(CMAKE_CXX_STANDARD 20)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
-
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-narrowing -Wall -Werror -Wno-dangling-else")
