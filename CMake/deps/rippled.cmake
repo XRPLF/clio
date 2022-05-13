f30ef6e29413db26c13741121f2b2f4f6be99113
Michael Legleux <mlegleux@ripple.com>
2022-05-13 09:53:11 -0700

    pin rippled 1.9.0

--- CMake/deps/rippled.cmake
+++ CMake/deps/rippled.cmake
@@ -1,5 +1,5 @@
 set(RIPPLED_REPO "https://github.com/ripple/rippled.git")
-set(RIPPLED_BRANCH "develop")
+set(RIPPLED_BRANCH "1.9.0")
 set(NIH_CACHE_ROOT "${CMAKE_CURRENT_BINARY_DIR}" CACHE INTERNAL "")
 message(STATUS "Cloning ${RIPPLED_REPO} branch ${RIPPLED_BRANCH}")
 FetchContent_Declare(rippled
