8a5be14ba8dbcee4e223b2d61ffc4323023199d0
Michael Legleux <mlegleux@ripple.com>
2022-05-16 11:53:13 -0700

    Fix clio package
    Configure example-config's clio.log path to /var/log/clio

--- CMake/install/install.cmake
+++ CMake/install/install.cmake
@@ -3,8 +3,14 @@ set(CMAKE_INSTALL_PREFIX ${CLIO_INSTALL_DIR})
 
 install(TARGETS clio_server DESTINATION bin)
 # install(TARGETS clio_tests DESTINATION bin) # NOTE: Do we want to install the tests?
-install(FILES example-config.json DESTINATION etc RENAME config.json)
+
+#install(FILES example-config.json DESTINATION etc RENAME config.json)
+file(READ example-config.json config)
+string(REGEX REPLACE "./clio.log" "/var/log/clio/clio.log" config "${config}")
+file(WRITE ${CMAKE_BINARY_DIR}/install-config.json "${config}")
+install(FILES ${CMAKE_BINARY_DIR}/install-config.json DESTINATION etc RENAME config.json)
 
 configure_file("${CMAKE_SOURCE_DIR}/CMake/install/clio.service.in" "${CMAKE_BINARY_DIR}/clio.service")
 
 install(FILES "${CMAKE_BINARY_DIR}/clio.service" DESTINATION /lib/systemd/system)
+
