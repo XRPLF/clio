ebc223f2f4c9133274f5667462a05d695f23a208
CJ Cobb <46455409+cjcobb23@users.noreply.github.com>
2022-11-21 15:52:59 -0600

    include searched_all in error response of tx (#407)

--- src/rpc/handlers/Tx.cpp
+++ src/rpc/handlers/Tx.cpp
@@ -31,6 +31,17 @@ doTx(Context const& context)
 
         binary = request.at(JS(binary)).as_bool();
     }
+    auto minLedger = getUInt(request, JS(min_ledger));
+    auto maxLedger = getUInt(request, JS(max_ledger));
+    bool rangeSupplied = minLedger && maxLedger;
+
+    if (rangeSupplied)
+    {
+        if (*minLedger > *maxLedger)
+            return Status{RippledError::rpcINVALID_LGR_RANGE};
+        if (*maxLedger - *minLedger > 1000)
+            return Status{RippledError::rpcEXCESSIVE_LGR_RANGE};
+    }
 
     auto range = context.backend->fetchLedgerRange();
     if (!range)
@@ -38,7 +49,17 @@ doTx(Context const& context)
 
     auto dbResponse = context.backend->fetchTransaction(hash, context.yield);
     if (!dbResponse)
+    {
+        if (rangeSupplied)
+        {
+            bool searchedAll = range->maxSequence >= *maxLedger &&
+                range->minSequence <= *minLedger;
+            boost::json::object extra;
+            extra["searched_all"] = searchedAll;
+            return Status{RippledError::rpcTXN_NOT_FOUND, std::move(extra)};
+        }
         return Status{RippledError::rpcTXN_NOT_FOUND};
+    }
 
     if (!binary)
     {
