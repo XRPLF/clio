50777a3c91eb4ca638cd9e99832770f6f21da824
Alex Kremer <akremer@ripple.com>
2022-12-06 15:53:14 +0000

    Fix source_location issue on MacOSX and Debug build (#431)
    Fixes #428

---
+++ CMake/deps/SourceLocation.cmake
@@ -0,0 +1,11 @@
+include(CheckIncludeFileCXX)
+
+check_include_file_cxx("source_location" SOURCE_LOCATION_AVAILABLE)
+if(SOURCE_LOCATION_AVAILABLE) 
+    target_compile_definitions(clio PUBLIC "HAS_SOURCE_LOCATION")
+endif()
+
+check_include_file_cxx("experimental/source_location" EXPERIMENTAL_SOURCE_LOCATION_AVAILABLE)
+if(EXPERIMENTAL_SOURCE_LOCATION_AVAILABLE)     
+    target_compile_definitions(clio PUBLIC "HAS_EXPERIMENTAL_SOURCE_LOCATION")
+endif()
