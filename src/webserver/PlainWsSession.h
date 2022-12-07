8a1f00debbb68343280630df2cc711ff95f52c54
CJ Cobb <46455409+cjcobb23@users.noreply.github.com>
2022-12-07 13:12:38 -0500

    add connection counting (#433)

--- src/webserver/PlainWsSession.h
+++ src/webserver/PlainWsSession.h
@@ -31,6 +31,7 @@ public:
     explicit PlainWsSession(
         boost::asio::io_context& ioc,
         boost::asio::ip::tcp::socket&& socket,
+        std::optional<std::string> ip,
         std::shared_ptr<BackendInterface const> backend,
         std::shared_ptr<SubscriptionManager> subscriptions,
         std::shared_ptr<ETLLoadBalancer> balancer,
@@ -42,6 +43,7 @@ public:
         boost::beast::flat_buffer&& buffer)
         : WsSession(
               ioc,
+              ip,
               backend,
               subscriptions,
               balancer,
@@ -64,19 +66,7 @@ public:
     std::optional<std::string>
     ip()
     {
-        try
-        {
-            return ws()
-                .next_layer()
-                .socket()
-                .remote_endpoint()
-                .address()
-                .to_string();
-        }
-        catch (std::exception const&)
-        {
-            return {};
-        }
+        return ip_;
     }
 
     ~PlainWsSession() = default;
@@ -97,11 +87,13 @@ class WsUpgrader : public std::enable_shared_from_this<WsUpgrader>
     RPC::Counters& counters_;
     WorkQueue& queue_;
     http::request<http::string_body> req_;
+    std::optional<std::string> ip_;
 
 public:
     WsUpgrader(
         boost::asio::io_context& ioc,
         boost::asio::ip::tcp::socket&& socket,
+        std::optional<std::string> ip,
         std::shared_ptr<BackendInterface const> backend,
         std::shared_ptr<SubscriptionManager> subscriptions,
         std::shared_ptr<ETLLoadBalancer> balancer,
@@ -122,11 +114,13 @@ public:
         , dosGuard_(dosGuard)
         , counters_(counters)
         , queue_(queue)
+        , ip_(ip)
     {
     }
     WsUpgrader(
         boost::asio::io_context& ioc,
         boost::beast::tcp_stream&& stream,
+        std::optional<std::string> ip,
         std::shared_ptr<BackendInterface const> backend,
         std::shared_ptr<SubscriptionManager> subscriptions,
         std::shared_ptr<ETLLoadBalancer> balancer,
@@ -149,6 +143,7 @@ public:
         , counters_(counters)
         , queue_(queue)
         , req_(std::move(req))
+        , ip_(ip)
     {
     }
 
@@ -197,6 +192,7 @@ private:
         std::make_shared<PlainWsSession>(
             ioc_,
             http_.release_socket(),
+            ip_,
             backend_,
             subscriptions_,
             balancer_,
--- src/webserver/WsBase.h
+++ src/webserver/WsBase.h
@@ -111,6 +111,9 @@ class WsSession : public WsBase,
     bool sending_ = false;
     std::queue<std::shared_ptr<Message>> messages_;
 
+protected:
+    std::optional<std::string> ip_;
+
     void
     wsFail(boost::beast::error_code ec, char const* what)
     {
@@ -128,6 +131,7 @@ class WsSession : public WsBase,
 public:
     explicit WsSession(
         boost::asio::io_context& ioc,
+        std::optional<std::string> ip,
         std::shared_ptr<BackendInterface const> backend,
         std::shared_ptr<SubscriptionManager> subscriptions,
         std::shared_ptr<ETLLoadBalancer> balancer,
@@ -148,12 +152,16 @@ public:
         , dosGuard_(dosGuard)
         , counters_(counters)
         , queue_(queue)
+        , ip_(ip)
     {
         perfLog_.info() << tag() << "session created";
     }
+
     virtual ~WsSession()
     {
         perfLog_.info() << tag() << "session closed";
+        if (ip_)
+            dosGuard_.decrement(*ip_);
     }
 
     // Access the derived class, this is part of
