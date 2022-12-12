e42f10a703b41291efd43ce38bcd1d20f393f39d
Alexander Kremer <akremer@ripple.com>
2022-12-12 20:09:21 +0000

    Add copyright to top of each source file

--- src/main/impl/Build.cpp
+++ src/main/impl/Build.cpp
@@ -1,3 +1,22 @@
+//------------------------------------------------------------------------------
+/*
+    This file is part of clio: https://github.com/XRPLF/clio
+    Copyright (c) 2022, the clio developers.
+
+    Permission to use, copy, modify, and distribute this software for any
+    purpose with or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
+
+    THE  SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH  REGARD  TO  THIS  SOFTWARE  INCLUDING  ALL  IMPLIED  WARRANTIES  OF
+    MERCHANTABILITY  AND  FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY  SPECIAL,  DIRECT,  INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER  RESULTING  FROM  LOSS  OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION  OF  CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+//==============================================================================
+
 #include <ripple/beast/core/SemanticVersion.h>
 #include <boost/preprocessor/stringize.hpp>
 #include <algorithm>
@@ -11,13 +30,10 @@ namespace Build {
 //  The build version number. You must edit this for each release
 //  and follow the format described at http://semver.org/
 //------------------------------------------------------------------------------
-// clang-format off
-char const* const versionString = "1.0.3"
-    // clang-format on
-
-    "+"
+char const* const versionString =
+    "1.0.3"
 #ifdef CLIO_BUILD
-    CLIO_BUILD
+    "+" CLIO_BUILD
 #endif
 #ifdef DEBUG
     ".DEBUG"
