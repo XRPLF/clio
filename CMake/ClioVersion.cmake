81bf9894e401ea1b47cbc58c12f0023e3cdc68c7
Alex Kremer <akremer@ripple.com>
2023-01-16 16:42:57 +0000

    Fix bug with ClioVersion to prevent crash at runtime (#473)
    Fixes #474

--- CMake/ClioVersion.cmake
+++ CMake/ClioVersion.cmake
@@ -4,18 +4,23 @@
 
 find_package(Git REQUIRED)
 
-execute_process(COMMAND ${GIT_EXECUTABLE} branch --show-current OUTPUT_VARIABLE BRANCH OUTPUT_STRIP_TRAILING_WHITESPACE)
+set(GIT_COMMAND rev-parse --short HEAD)
+execute_process(COMMAND ${GIT_EXECUTABLE} ${GIT_COMMAND} OUTPUT_VARIABLE REV OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-set(GIT_COMMAND describe --tags)
+set(GIT_COMMAND branch --show-current)
+execute_process(COMMAND ${GIT_EXECUTABLE} ${GIT_COMMAND} OUTPUT_VARIABLE BRANCH OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-execute_process(COMMAND ${GIT_EXECUTABLE} ${GIT_COMMAND} OUTPUT_VARIABLE VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+if(BRANCH STREQUAL "")
+  set(BRANCH "dev")
+endif()
 
-set(VERSION "${VERSION}")
-
-if(NOT (BRANCH MATCHES master OR BRANCH MATCHES release/*)) # for develop and any other branch name YYYYMMDDHMS-<git-ref>
-  set(GIT_COMMAND rev-parse --short HEAD)
+if(NOT (BRANCH MATCHES master OR BRANCH MATCHES release/*)) # for develop and any other branch name YYYYMMDDHMS-<branch>-<git-ref>
   execute_process(COMMAND date +%Y%m%d%H%M%S OUTPUT_VARIABLE DATE OUTPUT_STRIP_TRAILING_WHITESPACE)
-  set(VERSION "${VERSION}-${DATE}")
+  set(VERSION "${DATE}-${BRANCH}-${REV}")
+else()
+  set(GIT_COMMAND describe --tags)
+  execute_process(COMMAND ${GIT_EXECUTABLE} ${GIT_COMMAND} OUTPUT_VARIABLE TAG_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+  set(VERSION "${TAG_VERSION}-${REV}")
 endif()
 
 if(CMAKE_BUILD_TYPE MATCHES Debug)
