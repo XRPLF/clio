0454c98c933887675457e8e04d5238e9dc35a31f
ledhed2222 <ledhed2222@users.noreply.github.com>
2023-04-06 19:34:44 -0400

    add default ordering to issuer_nf_tokens_v2

--- src/backend/cassandra/Schema.h
+++ src/backend/cassandra/Schema.h
@@ -214,15 +214,18 @@ public:
 
         statements.emplace_back(fmt::format(
             R"(
-           CREATE TABLE IF NOT EXISTS {}
-                  ( 
-                      issuer blob,
-                       taxon bigint,
+            CREATE TABLE IF NOT EXISTS {}
+                ( 
+                    issuer blob,
+                    taxon bigint,
                     token_id blob,
-                     PRIMARY KEY (issuer, taxon, token_id)
-                  ) 
+                    PRIMARY KEY (issuer, taxon, token_id)
+                ) 
+                WITH CLUSTERING ORDER BY (taxon ASC, token_id ASC)
+                    AND default_time_to_live = {}
             )",
-            qualifiedTableName(settingsProvider_.get(), "issuer_nf_tokens_v2")));
+            qualifiedTableName(settingsProvider_.get(), "issuer_nf_tokens_v2"),
+            settingsProvider_.get().getTtl()));
 
         statements.emplace_back(fmt::format(
             R"(
