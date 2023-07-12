72668f9bf049dcfd427ab9a18196059dfa1b9c9d
cyan <cyan@ripple.com>
2023-07-12 11:58:39 +0100

    try

--- CMake/deps/rippled.cmake
+++ CMake/deps/rippled.cmake
@@ -2,6 +2,10 @@ target_link_directories(clio PUBLIC ${CONAN_LIB_DIRS_CLIO-XRPL})
 target_link_libraries(clio PUBLIC ${CONAN_LIBS_CLIO-XRPL})
 target_include_directories(clio PUBLIC ${CONAN_INCLUDE_DIRS_CLIO-XRPL})
 
+target_link_directories(clio PUBLIC ${CONAN_LIB_DIRS_ABSEIL})
+target_link_libraries(clio PUBLIC ${CONAN_LIBS_ABSEIL})
+target_include_directories(clio PUBLIC ${CONAN_INCLUDE_DIRS_ABSEIL})
+
 target_link_directories(clio PUBLIC ${CONAN_LIB_DIRS_GRPC} ${CONAN_LIB_DIRS_PROTOBUF})
 target_link_libraries(clio PUBLIC ${CONAN_LIBS_GRPC} ${CONAN_LIBS_PROTOBUF})
 target_include_directories(clio PUBLIC ${CONAN_INCLUDE_DIRS_GRPC} ${CONAN_INCLUDE_DIRS_PROTOBUF})
@@ -18,9 +22,6 @@ target_link_directories(clio PUBLIC ${CONAN_LIB_DIRS_ZLIB})
 target_link_libraries(clio PUBLIC ${CONAN_LIBS_ZLIB})
 target_include_directories(clio PUBLIC ${CONAN_INCLUDE_DIRS_ZLIB})
 
-target_link_directories(clio PUBLIC ${CONAN_LIB_DIRS_ABSEIL})
-target_link_libraries(clio PUBLIC ${CONAN_LIBS_ABSEIL})
-target_include_directories(clio PUBLIC ${CONAN_INCLUDE_DIRS_ABSEIL})
 
 target_link_directories(clio PUBLIC ${CONAN_LIB_DIRS_RE2})
 target_link_libraries(clio PUBLIC ${CONAN_LIBS_RE2})
