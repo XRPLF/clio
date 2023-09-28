963685dd31558a2e5c1617f50ab9ca3807622b16
cyan317 <120398799+cindyyan317@users.noreply.github.com>
2023-09-28 09:14:01 +0100

    Ledger_entry return invalid parameter error for v1 (#873)
    Fixes #875

--- src/rpc/handlers/LedgerEntry.cpp
+++ src/rpc/handlers/LedgerEntry.cpp
@@ -82,6 +82,8 @@ LedgerEntryHandler::process(LedgerEntryHandler::Input input, Context const& ctx)
     else
     {
         // Must specify 1 of the following fields to indicate what type
+        if (ctx.apiVersion == 1)
+            return Error{Status{RippledError::rpcINVALID_PARAMS}};
         return Error{Status{ClioError::rpcUNKNOWN_OPTION}};
     }
 
