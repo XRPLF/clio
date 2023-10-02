b93e4faf456ef993892833c8f81ce7350c6b2364
Sergey Kuznetsov <skuznetsov@ripple.com>
2023-10-02 16:18:16 +0100

    Remove useless cast

--- unittests/rpc/ForwardingProxyTests.cpp
+++ unittests/rpc/ForwardingProxyTests.cpp
@@ -296,7 +296,7 @@ TEST_F(RPCForwardingProxyTest, ShouldNotForwardReturnsTrueIfAPIVersionIsV1)
     auto const method = "api_version_check";
     auto const params = json::parse("{}");
 
-    auto const rawHandlerProviderPtr = static_cast<MockHandlerProvider*>(handlerProvider.get());
+    auto const rawHandlerProviderPtr = handlerProvider.get();
     ON_CALL(*rawHandlerProviderPtr, isClioOnly(_)).WillByDefault(Return(false));
     EXPECT_CALL(*rawHandlerProviderPtr, isClioOnly(method)).Times(1);
 
