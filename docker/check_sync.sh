4fd043facf2916cb04b19ebccd599cd362d991a3
Michael Legleux <mlegleux@ripple.com>
2023-11-01 20:13:21 -0700

    tweak healthcheck; add mainnet config

--- docker/check_sync.sh
+++ docker/check_sync.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/bash 
+#!/usr/bin/bash
 
-if [[ $(curl 127.0.0.1:5005 -sd'{"method": "server_info"}' | grep -o '"complete_ledgers":"[^"]*' | grep -o '[^"]*$') != "empty" ]]; then
+if [[ $(/opt/ripple/bin/rippled --silent --conf /opt/ripple/etc/rippled.cfg server_info | grep -o '"complete_ledgers" : "[^"]*' | grep -o '[^"]*$') != "empty" ]]; then
   echo "ready"
 else
   echo "Not ready!"
