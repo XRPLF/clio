5910f6a2529f05c31e072524f13b43b43cda34d8
Alex Kremer <akremer@ripple.com>
2023-12-18 22:11:46 +0000

    Redo benchmarks

--- benchmarks/Playground.cpp
+++ benchmarks/Playground.cpp
@@ -17,6 +17,29 @@
 */
 //==============================================================================
 
-#include <benchmark/benchmark.h>
+/*
+ * Use this file for temporary benchmarks and implementations.
+ * Usage example:
+ * ```
+ * ./clio_benchmarks
+ *     --benchmark_time_unit=ms
+ *     --benchmark_repetitions=10
+ *     --benchmark_display_aggregates_only=true
+ *     --benchmark_min_time=1x
+ *     --benchmark_filter="Playground"
+ * ```
+ *
+ * Note: Please don't push your temporary work to the repo.
+ */
 
-BENCHMARK_MAIN();
+// #include <benchmark/benchmark.h>
+
+// static void
+// benchmarkPlaygroundTest1(benchmark::State& state)
+// {
+//     for (auto _ : state) {
+//         // ...
+//     }
+// }
+
+// BENCHMARK(benchmarkPlaygroundTest1);
