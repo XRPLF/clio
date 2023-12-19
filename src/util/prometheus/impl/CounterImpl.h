10071d984f97b480f1efbd7e2c4b45b994b6f18f
Bronek Kozicki <brok@incorrekt.com>
2023-12-19 19:09:50 +0000

    Fix c++20 requires syntax

--- src/util/prometheus/impl/CounterImpl.h
+++ src/util/prometheus/impl/CounterImpl.h
@@ -30,7 +30,7 @@ namespace util::prometheus::detail {
 template <typename T>
 concept SomeCounterImpl = requires(T a) {
     typename std::remove_cvref_t<T>::ValueType;
-    SomeNumberType<typename std::remove_cvref_t<T>::ValueType>;
+    requires SomeNumberType<typename std::remove_cvref_t<T>::ValueType>;
     {
         a.add(typename std::remove_cvref_t<T>::ValueType{1})
     } -> std::same_as<void>;
