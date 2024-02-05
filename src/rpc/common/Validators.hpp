f3d7f0df491f66788e261792f5ed6ed6daa3b718
Shawn Xie <shawnxie920@gmail.com>
2024-02-05 13:09:26 -0500

    template parameter type checking

--- src/rpc/common/Validators.hpp
+++ src/rpc/common/Validators.hpp
@@ -458,6 +458,9 @@ public:
 checkIsU32Numeric(std::string_view sv);
 
 template <class HexType>
+requires (std::is_same_v<HexType, ripple::uint160> ||
+            std::is_same_v<HexType, ripple::uint192> ||
+            std::is_same_v<HexType, ripple::uint256> )
 MaybeError makeHexStringValidator(boost::json::value const& value, std::string_view key) {
     if (!value.is_string())
         return Error{Status{RippledError::rpcINVALID_PARAMS, std::string(key) + "NotString"}};
