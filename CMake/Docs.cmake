94a7805ee65d44148998b4d2bb644dee18273a75
Alex Kremer <akremer@ripple.com>
2024-03-02 00:16:10 +0000

    Add simple util to gather doxygen warnings

--- CMake/Docs.cmake
+++ CMake/Docs.cmake
@@ -1,9 +1,10 @@
 find_package(Doxygen REQUIRED)
 
+set(SOURCE ${CMAKE_CURRENT_SOURCE_DIR})
 set(DOXYGEN_IN ${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile)
 set(DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
 
-configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
+configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT})
 add_custom_target(
   docs
   COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
