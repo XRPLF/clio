4a02330f43881dc24dadde9999ca6d6513f3cf10
Sergey Kuznetsov <skuznetsov@ripple.com>
2024-05-23 12:21:11 +0100

    Some code improvements

--- tools/requests_gun/internal/trigger/trigger.go
+++ tools/requests_gun/internal/trigger/trigger.go
@@ -1,7 +1,7 @@
 package trigger
 
 import (
-	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"requests_gun/internal/ammo_provider"
@@ -13,43 +13,41 @@ import (
 )
 
 func Fire(ammoProvider *ammo_provider.AmmoProvider, args *parse_args.CliArgs) {
-	stop := atomic.Bool{}
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
-	go func() {
-
-		s := <-interrupt
-		fmt.Println("Got signal:", s)
-		fmt.Println("Stopping...")
-		stop.Store(true)
-	}()
-
 	wg := sync.WaitGroup{}
-	for !stop.Load() {
-		statistics := statistics{startTime: time.Now(), printErrors: args.PrintErrors}
-		do_shot := func() {
-			defer wg.Done()
-			bullet := ammoProvider.GetBullet()
-            requestMaker := request_maker.NewHttp(args.Url, args.Port)
-			response_data, err := requestMaker.MakeRequest(bullet)
-			statistics.add(response_data, err)
-		}
+	ticker := time.NewTicker(time.Second)
+	for {
+		select {
+		case s := <-interrupt:
+			log.Println("Got signal:", s)
+			log.Println("Stopping...")
+			ticker.Stop()
+			return
+		case <-ticker.C:
+			statistics := statistics{startTime: time.Now(), printErrors: args.PrintErrors}
+			do_shot := func() {
+				defer wg.Done()
+				bullet := ammoProvider.GetBullet()
+				requestMaker := request_maker.NewHttp(args.Url, args.Port)
+				response_data, err := requestMaker.MakeRequest(bullet)
+				statistics.add(response_data, err)
+			}
 
-		second_start := time.Now()
-		requests_number := uint(0)
-		for requests_number < args.TargetLoad && time.Since(second_start) < time.Second {
-			wg.Add(1)
-			go do_shot()
-			requests_number++
+			second_start := time.Now()
+			requests_number := uint(0)
+			for requests_number < args.TargetLoad && time.Since(second_start) < time.Second {
+				wg.Add(1)
+				go do_shot()
+				requests_number++
+			}
+			wg.Wait()
+			if time.Since(second_start) > time.Second {
+				log.Println("Requests took longer than a second, probably need to decrease the load.")
+			}
+			statistics.print()
 		}
-		wg.Wait()
-		if time.Since(second_start) < time.Second {
-			time.Sleep(time.Second - time.Since(second_start))
-		} else {
-			fmt.Println("Requests took longer than a second, probably need to decrease the load.")
-		}
-		statistics.print()
 	}
 }
 
@@ -69,18 +67,18 @@ func (s *statistics) add(response *request_maker.ResponseData, err error) {
 	s.counters.totalRequestsCount.Add(1)
 	if err != nil {
 		if s.printErrors {
-			fmt.Println("Error making request:", err)
+			log.Println("Error making request:", err)
 		}
 		s.counters.errorsCount.Add(1)
 		return
 	}
 	if response.StatusCode != 200 || response.Body["error"] != nil {
 		if s.printErrors {
-			fmt.Print("Response contains error: ", response.StatusStr)
+			log.Print("Response contains error: ", response.StatusStr)
 			if response.Body["error"] != nil {
-				fmt.Println(" ", response.Body["error"])
+				log.Println(" ", response.Body["error"])
 			} else {
-				fmt.Println()
+				log.Println()
 			}
 		}
 		s.counters.badReplyCount.Add(1)
@@ -94,7 +92,7 @@ func (s *statistics) print() {
 	if elapsed < time.Second {
 		elapsed = time.Second
 	}
-	fmt.Printf("Speed: %.1f rps, Errors: %.1f%%, Bad response: %.1f%%, Good response: %.1f%%\n",
+	log.Printf("Speed: %.1f rps, Errors: %.1f%%, Bad response: %.1f%%, Good response: %.1f%%\n",
 		float64(s.counters.totalRequestsCount.Load())/elapsed.Seconds(),
 		float64(s.counters.errorsCount.Load())/float64(s.counters.totalRequestsCount.Load())*100,
 		float64(s.counters.badReplyCount.Load())/float64(s.counters.totalRequestsCount.Load())*100,
