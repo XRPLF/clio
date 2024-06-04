c1037785a11d5079ab6b8a72f1585a0e9ff2810a
Sergey Kuznetsov <skuznetsov@ripple.com>
2024-06-04 16:02:28 +0100

    Upgrade xrpl to 2.2.0 (#1443)

--- conanfile.py
+++ conanfile.py
@@ -26,7 +26,7 @@ class Clio(ConanFile):
         'protobuf/3.21.9',
         'grpc/1.50.1',
         'openssl/1.1.1u',
-        'xrpl/2.2.0-rc3',
+        'xrpl/2.2.0',
         'libbacktrace/cci.20210118'
     ]
 
