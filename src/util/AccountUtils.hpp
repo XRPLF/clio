ae7e1a93c24c21e456550b208266da83c2e61440
cyan <cyan@ripple.com>
2024-07-15 16:31:53 +0100

    Add space

--- src/util/AccountUtils.hpp
+++ src/util/AccountUtils.hpp
@@ -29,11 +29,11 @@ namespace util {
 
 /**
  * @brief A wrapper of parseBase58 function. It adds the check if all characters in the input string are alphanumeric.
- *If not, it returns an empty optional, instead of calling the parseBase58 function.
+ * If not, it returns an empty optional, instead of calling the parseBase58 function.
  *
- *@tparam T The type of the value to parse to.
- *@param str The string to parse.
- *@return An optional with the parsed value, or an empty optional if the parse fails.
+ * @tparam T The type of the value to parse to.
+ * @param str The string to parse.
+ * @return An optional with the parsed value, or an empty optional if the parse fails.
  */
 template <class T>
 [[nodiscard]] std::optional<T>
@@ -47,12 +47,12 @@ parseBase58Wrapper(std::string const& str)
 
 /**
  * @brief A wrapper of parseBase58 function. It add the check if all characters in the input string are alphanumeric. If
- *not, it returns an empty optional, instead of calling the parseBase58 function.
+ * not, it returns an empty optional, instead of calling the parseBase58 function.
  *
- *@tparam T The type of the value to parse to.
- *@param type The type of the token to parse.
- *@param str The string to parse.
- *@return An optional with the parsed value, or an empty optional if the parse fails.
+ * @tparam T The type of the value to parse to.
+ * @param type The type of the token to parse.
+ * @param str The string to parse.
+ * @return An optional with the parsed value, or an empty optional if the parse fails.
  */
 template <class T>
 [[nodiscard]] std::optional<T>
