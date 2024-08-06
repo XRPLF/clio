8def9c976f313776f115016ad7c48e72975f19d8
kuznetsss <15742918+kuznetsss@users.noreply.github.com>
2024-08-06 06:32:39 +0000

    [CI] clang-tidy auto fixes

--- src/rpc/handlers/LedgerData.cpp
+++ src/rpc/handlers/LedgerData.cpp
@@ -33,7 +33,6 @@
 #include <boost/json/value_to.hpp>
 #include <xrpl/basics/base_uint.h>
 #include <xrpl/basics/strHex.h>
-#include <xrpl/protocol/ErrorCodes.h>
 #include <xrpl/protocol/LedgerFormats.h>
 #include <xrpl/protocol/LedgerHeader.h>
 #include <xrpl/protocol/STLedgerEntry.h>
