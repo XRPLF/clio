6482ada04107dbf9ca5b020c8cb72efe3f440fe8
Sergey Kuznetsov <skuznetsov@ripple.com>
2025-03-04 16:13:57 +0000

    Fix review comment

--- src/data/CassandraBackend.hpp
+++ src/data/CassandraBackend.hpp
@@ -120,7 +120,9 @@ public:
         LOG(log_.info()) << "Created (revamped) CassandraBackend";
     }
 
-    // Move constructor is deleted because handle_ is shared by reference with executor_
+    /*
+     * @brief Move constructor is deleted because handle_ is shared by reference with executor
+     */
     BasicCassandraBackend(BasicCassandraBackend&&) = delete;
 
     TransactionsAndCursor
