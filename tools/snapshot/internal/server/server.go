658b15a6486cd78edef720dbbfd83acc92163d73
cyan <cyan@ripple.com>
2025-03-21 11:13:12 +0000

    order change

--- tools/snapshot/internal/server/server.go
+++ tools/snapshot/internal/server/server.go
@@ -30,13 +30,11 @@ func StartServer(grpcServerAddr string, wsServerAddr string, path string) {
 	pb.RegisterXRPLedgerAPIServiceServer(grpcServer, newServer(path))
 	log.Print("Starting server...")
 	go grpcServer.Serve(lis)
-	// Create ledger stream reply
 
-	ledgerStreamReply := fmt.Sprintf("{\"fee_base\":10,\"ledger_hash\":\"A320C67DA7D1250A577AC5AACDF06ADC25E0EEEF7AE5B8D63CE2E1CC7F76A438\",\"ledger_index\":%d,\"ledger_time\":792853443,\"reserve_base\":1000000,\"reserve_inc\":200000,\"txn_count\":0,\"type\":\"ledgerClosed\",\"validated_ledgers\":\"%d-%d\"}",
-		endSeq, startSeq, endSeq)
-
-	log.Println(ledgerStreamReply)
 	wsServer := NewWebSocketServer("Snapshot Server", func(message string) string {
+		//mimic the response of the ledger stream
+		ledgerStreamReply := fmt.Sprintf("{\"fee_base\":10,\"ledger_hash\":\"A320C67DA7D1250A577AC5AACDF06ADC25E0EEEF7AE5B8D63CE2E1CC7F76A438\",\"ledger_index\":%d,\"ledger_time\":792853443,\"reserve_base\":1000000,\"reserve_inc\":200000,\"txn_count\":0,\"type\":\"ledgerClosed\",\"validated_ledgers\":\"%d-%d\"}",
+			endSeq, startSeq, endSeq)
 		return ledgerStreamReply
 	})
 	wsServer.Start(wsServerAddr)
