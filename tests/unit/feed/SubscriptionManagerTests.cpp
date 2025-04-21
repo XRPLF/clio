f61b36cf78f793dd9e6cbf008e7cf5a1624964b5
Peter Chen <ychen@ripple.com>
2025-04-21 17:40:14 -0400

    add setgetID test

--- tests/unit/feed/SubscriptionManagerTests.cpp
+++ tests/unit/feed/SubscriptionManagerTests.cpp
@@ -79,6 +79,12 @@ using SubscriptionManagerTest = SubscriptionManagerBaseTest<util::async::SyncExe
 
 using SubscriptionManagerAsyncTest = SubscriptionManagerBaseTest<util::async::PoolExecutionContext>;
 
+TEST_F(SubscriptionManagerAsyncTest, SetAndGetNetworkID)
+{
+    subscriptionManagerPtr_->setNetworkID(32u);
+    EXPECT_EQ(subscriptionManagerPtr_->getNetworkID(), 32u);
+}
+
 TEST_F(SubscriptionManagerAsyncTest, MultipleThreadCtx)
 {
     EXPECT_CALL(*sessionPtr_, onDisconnect);
