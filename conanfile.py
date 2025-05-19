62feda242105b79f0c6921105746e4bc44e33788
Sergey Kuznetsov <skuznetsov@ripple.com>
2025-05-19 16:16:09 +0100

    Use libxrpl 2.5.0-b1

--- conanfile.py
+++ conanfile.py
@@ -31,7 +31,7 @@ class Clio(ConanFile):
         'protobuf/3.21.9',
         'grpc/1.50.1',
         'openssl/1.1.1v',
-        'xrpl/2.4.0@clio/permission_delegation',
+        'xrpl/2.5.0-b1',
         'zlib/1.3.1',
         'libbacktrace/cci.20210118'
     ]
