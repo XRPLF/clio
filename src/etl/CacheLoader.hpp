69417adcd7397c37dc09c1b8b308be26cb78eca0
Sergey Kuznetsov <skuznetsov@ripple.com>
2026-03-05 17:25:09 +0000

    Move code into a separate method

--- src/etl/CacheLoader.hpp
+++ src/etl/CacheLoader.hpp
@@ -104,16 +104,7 @@ public:
 
         if (loadCacheFromFile()) {
             // Cache file may contain outdated data, so fetch whatever left up to seq from DB
-            while (cache_.get().latestLedgerSequence() < seq) {
-                auto seqToLoad = cache_.get().latestLedgerSequence() + 1;
-                LOG(log_.info()) << "Fetching ledger " << seqToLoad
-                                 << "from DB after loading cache from file";
-                auto const diff = data::synchronousAndRetryOnTimeout([this, seqToLoad](auto yield) {
-                    return backend_->fetchLedgerDiff(seqToLoad, yield);
-                });
-                cache_.get().update(diff, seqToLoad);
-                LOG(log_.info()) << "Updated cache to " << seqToLoad;
-            }
+            updateCacheToSeq(seq);
             cache_.get().setFull();
             return;
         }
@@ -205,6 +196,21 @@ private:
                          << " ms. Latest sequence: " << cache_.get().latestLedgerSequence();
         // backend_->forceUpdateRange(cache_.get().latestLedgerSequence());
         return true;
+    }
+
+    void
+    updateCacheToSeq(uint32_t const seq)
+    {
+        while (cache_.get().latestLedgerSequence() < seq) {
+            auto seqToLoad = cache_.get().latestLedgerSequence() + 1;
+            LOG(log_.info()) << "Fetching ledger " << seqToLoad
+                             << "from DB after loading cache from file";
+            auto const diff = data::synchronousAndRetryOnTimeout([this, seqToLoad](auto yield) {
+                return backend_->fetchLedgerDiff(seqToLoad, yield);
+            });
+            cache_.get().update(diff, seqToLoad);
+            LOG(log_.info()) << "Updated cache to " << seqToLoad;
+        }
     }
 };
 
