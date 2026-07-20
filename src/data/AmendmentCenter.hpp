435790e78763bb30f8f666932818afcfd9676015
Ayaz Salikhov <mathbunnyru@gmail.com>
2026-07-20 19:36:23 +0100

    Add comment

--- src/data/AmendmentCenter.hpp
+++ src/data/AmendmentCenter.hpp
@@ -139,6 +139,7 @@ struct Amendments {
     REGISTER(fixBatchInnerSigs);
     REGISTER(fixCleanup3_1_3);
     REGISTER(fixCleanup3_2_0);
+    // These amendments are added because of libxrpl 3.3.0, but they are not yet supported in Clio.
     REGISTER(ConfidentialTransfer);
     REGISTER(LendingProtocolV1_1);
     REGISTER(BatchV1_1);
